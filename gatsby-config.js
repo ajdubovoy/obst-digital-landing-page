c3f862365710aa8a0b4cdc358a875d35bf72e816
Alexander Dubovoy <ajdubovoy@gmail.com>
2019-08-22 12:43:20 +0200

    some inital tweaks

--- gatsby-config.js
+++ gatsby-config.js
@@ -1,7 +1,7 @@
 module.exports = {
   siteMetadata: {
-    title: `Gatsby Default Starter`,
-    description: `Kick off your next, great Gatsby project with this default starter. This barebones starter ships with the main Gatsby configuration files you might need.`,
+    title: `Obst Digital`,
+    description: `We build and design digital projects out of Berlin, Germany.`,
     author: `@gatsbyjs`,
   },
   plugins: [
